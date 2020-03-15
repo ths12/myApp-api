16697bbb39ae4f0eba70b1a71755ad92da70f115
ths12 <tomasz.halas7@gmail.com>
2020-03-15 18:17:33 +0100

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-encircled-30474',
     user : 'postgres',
     password : 'DarkMode4&',
     database : 'smart-brain'
